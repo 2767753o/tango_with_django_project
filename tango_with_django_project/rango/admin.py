8271fa48bd23256ff81654884928e4b5d01450ad
“John <2767753o@student.gla.ac.uk>
2024-02-07 21:21:40 +0000

    chapter 5 done

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+# admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    # def __init__(self):
+    #     super.__init__()
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
