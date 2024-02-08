7b69da18a5299d6aaa687fa12ac156a4c0d433bf
“John <2767753o@student.gla.ac.uk>
2024-02-08 00:39:37 +0000

    chapter 6 complete

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -13,40 +13,48 @@ def populate():
         {
             'title': 'Official Python Tutorial',
             'url': 'http://docs.python.org/3/tutorial/',
+            'views': 21,
         },
         {
             'title': 'How to Think like a Computer Scientist',
             'url': 'http://www.greenteapress.com/thinkpython/',
+            'views': 12,
         },
         {
             'title': 'Learn Python in 10 Minutes',
             'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views': 2112,
         },
     ]
 
     django_pages = [
         {
             'title': 'Official Django Tutorial',
-            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'
+            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 32,
         },
         {
             'title': 'Django Rocks',
-            'url': 'http://www.djangorocks.com/'
+            'url': 'http://www.djangorocks.com/',
+            'views': 420,
         },
         {
             'title': 'How to Tango with Django',
-            'url': 'http://www.tangowithdjango.com/'
+            'url': 'http://www.tangowithdjango.com/',
+            'views': 69,
         },
     ]
 
     other_pages = [
         {
             'title': 'Bottle',
-            'url': 'http://bottlepy.org/docs/dev/'
+            'url': 'http://bottlepy.org/docs/dev/',
+            'views': 9000,
         },
         {
             'title': 'Flask',
-            'url': 'http://flask.pocoo.org'
+            'url': 'http://flask.pocoo.org',
+            'views': 4,
         },
     ]
 
@@ -71,7 +79,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(cat=c, title=p['title'], url=p['url'], views=p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-    # def __init__(self):
-    #     super.__init__()
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
